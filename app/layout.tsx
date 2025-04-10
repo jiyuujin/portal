3fa17b76590b0c7df0d1b102311587fb96686237
jiyuujin <y-kitamura@wevnal.co.jp>
2025-04-11 08:27:19 +0900

    fix: OGP 情報を修正 (画像)

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,19 @@ export const metadata = {
   title: "YUMA Kitamura - Application Developer & Engineering Manager",
   description:
     "Personal portfolio of YUMA Kitamura, an application developer and engineering manager specializing in frontend development with React, Flutter, and DevOps.",
+  openGraph: {
+    title: "YUMA Kitamura - Application Developer & Engineering Manager",
+    description:
+      "Personal portfolio of YUMA Kitamura, an application developer and engineering manager specializing in frontend development with React, Flutter, and DevOps.",
+    images: [
+      {
+        url: "https://i.imgur.com/7XpnwSH.jpg",
+        width: 1200,
+        height: 630,
+        alt: "",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
