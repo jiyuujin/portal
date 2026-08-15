852de7e9b53f234701795b251e75012cd291d3c9
jiyuujin <y-kitamura@wevnal.co.jp>
2026-08-15 13:40:13 +0900

    fix: Product 表記を Brand 表記に修正

--- app/page.tsx
+++ app/page.tsx
@@ -606,7 +606,7 @@ export default async function Home() {
                         rel="noopener noreferrer"
                       >
                         <ExternalLink className="h-4 w-4 mr-2" />
-                        Product Website
+                        Brand Website
                       </Link>
                     </Button>
                   </div>
